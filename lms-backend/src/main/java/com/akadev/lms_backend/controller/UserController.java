18450a9b205067d346dfa66197ad62171fba08cb
Moses Ayite <ayitemoses@gmail.com>
2026-09-04 12:29:29 -0400

    feat: add user controller

---
+++ lms-backend/src/main/java/com/akadev/lms_backend/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.akadev.lms_backend.controller;
+
+import com.akadev.lms_backend.dto.UserDto;
+import com.akadev.lms_backend.service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+
+    // CREATE API
+
+    @PostMapping
+    public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto) {
+        UserDto savedUser = userService.createUser(userDto);
+        return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
+    }
+
+}
